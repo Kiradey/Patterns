c6698619607e717a77c69356494b4cd1da9c5e9f
Kiradey <alex.sura143@gmail.com>
2023-12-14 09:34:09 +0200

    changed

---
+++ Memento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Memento
+{
+    class Program
+    {
+        static void Main()
+        {
+            TextEditorMemento textEditor = new("Hello World!");
+            TextEditorMemento textEditor2 = new("Hello Moon");
+
+            TextEditor editor = new(textEditor);
+            TextEditor editor2 = new(textEditor2);
+
+            Memento editorMemento = editor.CreateMemento();
+            Memento editorMemento1 = editor2.CreateMemento();
+            Store store = new();
+            store.Add(editorMemento);
+            Memento previousMemento = store.Previous();
+            store.Add(editorMemento1);
+            TextEditor restoredEditor = new(previousMemento);
+            Console.WriteLine(restoredEditor.GetState());
+
+            Console.ReadLine();
+        }
+    }
+}
